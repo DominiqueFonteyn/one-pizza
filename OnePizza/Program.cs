4f66d9872c1260720cf76beb6de680dda37f37eb
Dominique Fonteyn <dominique.fonteyn@protime.eu>
2022-01-15 21:35:08 +0100

    program outline

--- OnePizza/Program.cs
+++ OnePizza/Program.cs
@@ -1,14 +1,36 @@
 ﻿using System;
+using System.Linq;
 
 namespace OnePizza
 {
-    class Program
+    internal class Program
     {
         // https://codingcompetitions.withgoogle.com/hashcode/round/00000000008f5ca9/00000000008f6f33
-        
-        static void Main(string[] args)
+
+        private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            
+            string[] input =
+            {
+                "3",
+                "2 cheese peppers",
+                "0",
+                "1 basil",
+                "1 pineapple",
+                "2 mushrooms tomatoes",
+                "1 basil"
+            };
+
+            var clients = InputParser.Parse(input).ToArray();
+
+            var ingredients = clients.SelectMany(x => x.Likes.Union(x.Dislikes)).Distinct().OrderBy(x => x).ToArray();
+
+            var pizzas = PizzaGenerator.CreatePizzas(ingredients);
+            
+            // TODO score each pizza
+            
+            Console.WriteLine("Bye!");
         }
     }
 }
