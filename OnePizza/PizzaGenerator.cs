c1c95ff15f667143f1acbf79ffe22ac7ca549e6b
Dominique Fonteyn <dominique.fonteyn@protime.eu>
2022-01-15 21:28:07 +0100

    PizzaGenerator stub

---
+++ OnePizza/PizzaGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OnePizza
+{
+    public class PizzaGenerator
+    {
+        public static IEnumerable<string[]> CreatePizzas(string[] ingredients)
+        {
+            // TODO generate all possible combinations
+            throw new NotImplementedException();
+        }
+    }
+}
