866a8ae17bd5cddc680c82941649298210e449bd
Dominique Fonteyn <dominique.fonteyn@protime.eu>
2022-01-15 21:21:55 +0100

    client likes pizza

---
+++ OnePizza.Tests/ClientTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace OnePizza.Tests
+{
+    public class ClientTest
+    {
+        public static IEnumerable<object[]> Data => new[]
+        {
+            new object[]
+            {
+                new[] { "cheese", "peppers" },
+                Array.Empty<string>(),
+                true
+            },
+            new object[]
+            {
+                new[] { "basil" },
+                new[] { "pineapple" },
+                false
+            },
+            new object[]
+            {
+                new[] { "mushrooms", "tomatoes" },
+                new[] { "basil" },
+                true
+            }
+        };
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void LikesPizza(string[] likes, string[] dislikes, bool expected)
+        {
+            var pizzaIngredients = new[] { "cheese", "mushrooms", "tomatoes", "peppers" };
+
+            Assert.Equal(expected, new Client(likes, dislikes).LikesPizza(pizzaIngredients));
+        }
+    }
+}
--- OnePizza/Client.cs
+++ OnePizza/Client.cs
@@ -1,14 +1,31 @@
-﻿namespace OnePizza
+﻿using System.Linq;
+
+namespace OnePizza
 {
     public class Client
     {
-        public string[] Likes { get; }
-        public string[] Dislikes { get; }
-
         public Client(string[] likes, string[] dislikes)
         {
             Likes = likes;
             Dislikes = dislikes;
+        }
+
+        public string[] Likes { get; }
+        public string[] Dislikes { get; }
+
+        public bool LikesPizza(string[] ingredients)
+        {
+            return LikesAllIngredients(ingredients) && HasNoDislikedIngredients(ingredients);
+        }
+
+        private bool HasNoDislikedIngredients(string[] ingredients)
+        {
+            return !Dislikes.Intersect(ingredients).Any();
+        }
+
+        private bool LikesAllIngredients(string[] ingredients)
+        {
+            return Likes.Intersect(ingredients).Count() == Likes.Length;
         }
     }
 }
