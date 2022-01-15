c8e0742dea454d2aaa593903a22dcce0a1723b00
Dominique Fonteyn <dominique.fonteyn@protime.eu>
2022-01-15 20:16:42 +0100

    initial commit

---
+++ OnePizza/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OnePizza
+{
+    class Program
+    {
+        // https://codingcompetitions.withgoogle.com/hashcode/round/00000000008f5ca9/00000000008f6f33
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
