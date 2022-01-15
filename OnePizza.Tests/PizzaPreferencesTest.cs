3942262881c74b81467874fbf523ab9567030f0e
Dominique Fonteyn <dominique.fonteyn@protime.eu>
2022-01-15 20:27:20 +0100

    read NumberOfPotentialClients from input file

---
+++ OnePizza.Tests/PizzaPreferencesTest.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+
+namespace OnePizza.Tests
+{
+    public class PizzaPreferencesTest
+    {
+        private readonly string[] _input =
+        {
+            "3",
+            "2 cheese peppers",
+            "0",
+            "1 basil",
+            "1 pineapple",
+            "2 mushrooms tomatoes",
+            "1 basil"
+        };
+
+        [Fact]
+        public void Ctor_Parses_NumberOfPotentialClients()
+        {
+            var preferences = new PizzaPreferences(_input);
+            Assert.Equal(3, preferences.NumberOfPotentialClients);
+        }
+    }
+}
---
+++ OnePizza/PizzaPreferences.cs
@@ -0,0 +1,12 @@
+﻿namespace OnePizza
+{
+    public class PizzaPreferences
+    {
+        public PizzaPreferences(string[] clientPreferences)
+        {
+            NumberOfPotentialClients = int.Parse(clientPreferences[0]);
+        }
+
+        public int NumberOfPotentialClients { get; }
+    }
+}
